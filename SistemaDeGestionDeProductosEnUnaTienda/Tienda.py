e28fc1b62b55ab4bb77ae1b15d18d495aba2e7a4
leoardo4390 <tidus4390@gmail.com>
2026-09-18 20:02:31 -0300

    agregando codigo base

---
+++ SistemaDeGestionDeProductosEnUnaTienda/Tienda.py
@@ -0,0 +1,23 @@
+from producto import Producto
+
+class Tienda:
+    def __init__(self):
+        self.inventario = []
+    
+    def agregar_producto(self, producto):
+        self.inventario.append(producto)
+        
+    def buscar_producto(self,nombre):
+        for producto in self.inventario:
+            if producto.nombre == nombre:
+                return producto
+        
+        return None
+    
+    def eliminar_producto(self, nombre):
+        for producto in self.inventario:
+            if producto.nombre == nombre:
+                self.inventario.remove(producto)
+                return True
+        
+        return False
---
+++ SistemaDeGestionDeProductosEnUnaTienda/producto.py
@@ -0,0 +1,6 @@
+class Producto:
+    def __int__(self, nombre, precio, categoria):
+        self.nombre = nombre
+        self.precio = precio
+        self.categoria = categoria
+    
